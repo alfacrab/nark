5e167009dc1a0a25eeabd762df34ac9177292364
Stanislav Zholudev <zldev@mail.ru>
2026-08-19 23:37:28 +0300

    wip

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -31,6 +31,23 @@ func run() error {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
+	grpcErrs := make(chan error, 1)
+	go func() {
+		log.Info("grpc server listening",
+			slog.String("addr", cfg.GRPCAddr),
+			slog.Int("workers", cfg.Pool.Workers),
+			slog.Int("queue_size", cfg.Pool.QueueSize),
+			slog.String("topic", cfg.Producer.Topic),
+		)
+
+		// if err := grpcServer.Serve(listener); err != nil {
+		// 	grpcErrs <- err
+		// 	return
+		// }
+
+		close(grpcErrs)
+	}()
+
 	var runErr error
 	<-ctx.Done()
 	log.Info("shutdown signal received")
